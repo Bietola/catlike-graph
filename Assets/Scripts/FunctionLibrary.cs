7533905fd1435a344f525ad9ce2467982ec6f65d
Bietola <dincio.montesi@gmail.com>
2020-10-21 10:08:49 +0200

    Make function move around

---
+++ Assets/Scripts/FunctionLibrary.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.Mathf;
+
+public class FunctionLibrary : MonoBehaviour
+{
+    static public float SimpleWave(float x, float _z, float t) {
+        float output = Sin(PI * (x + t));
+
+        return output;
+    }
+
+    static public float Wave(float x, float _z, float t) {
+        float output = Sin(PI * (x + t)) + 0.5f * Sin(2f * PI * (x + t));
+        output /= 1.5f;
+
+        return output;
+    }
+}
--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -6,38 +6,60 @@ using UnityEngine;
 public class Graph : MonoBehaviour
 {
     [SerializeField]
-    Transform transformPrefab = default;
+    Transform pointPrefab = default;
 
     [SerializeField, Range(10, 100)]
-    int resolution;
+    int resolution = 25;
 
-    private void plotFunc(Func<int, int> func) {
+    [SerializeField, Range(0, 1)]
+    int functionId = 0;
+
+    List<Transform> points = new List<Transform>();
+
+    private void plotFunc(Func<float, float, float, float> func, float t) {
         // NB. Floor division forces even res. (kept for simplicity)
         var bounds = resolution / 2;
 
-        for (int i = -bounds; i < bounds; i++) {
-            plotPoint(i, func(i));
+        for (int i = 0; i < resolution; i++) {
+            // To make the domain from [0, 1] to [-0.5, 0.5]
+            int shifted_i = i - resolution / 2;
+
+            // Scale coordinated to fit into domain
+            float x = (float)shifted_i / resolution;
+            float y = func(x, 0, t);
+
+            // Modify position of cached point
+            points[i].localPosition = Vector3.right * x + Vector3.up * y;
         }
-    }
-
-    private void plotPoint(int x, int y) {
-        var point = Instantiate(transformPrefab);
-
-        // Bind Point to Graph transform
-        point.SetParent(transform);
-
-        // Scale position and dimensions appropiately
-        point.localPosition = (Vector3.right * x + Vector3.up * y) / resolution;
-        point.localScale = Vector3.one / resolution;
     }
 
     void Awake()
     {
-        plotFunc(x => x);
+        // Allocate all needed point prefabs in memory
+        for (int i = 0; i < resolution; i++) {
+            var point = Instantiate(pointPrefab);
+            point.SetParent(transform);
+
+            point.localScale = Vector3.one / resolution;
+
+            points.Add(point);
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        // TODO find out how type aliaes work
+        Func<float, float, float, float> funToPlot = FunctionLibrary.SimpleWave;
+
+        if (functionId == 0) {
+            funToPlot = FunctionLibrary.SimpleWave;
+        } else if (functionId == 1) {
+            funToPlot = FunctionLibrary.Wave;
+        } else {
+            Debug.Assert(false, "Impossible function ID");
+        }
+
+        plotFunc(funToPlot, Time.fixedTime);
     }
 }
